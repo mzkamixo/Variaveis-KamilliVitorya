159b4276edc987071b9ec30384b63e3e9424c71f
mzkamixo <167372860+mzkamixo@users.noreply.github.com>
2024-08-29 01:29:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let anoAtual = 2024;
+const nomeCompleto = "Kamilli Vitorya Borgmann de Oliveira";
+let idade = 16;
+const cursoFaculdade = "Letras habilitação em inglês";
+let cidadeAtual = "SJP";
+
+console.log(` Olá, eu sou a ${nomeCompleto}, tenho ${idade} anos, estou no ensino médio, recém me mudei para ${cidadeAtual}, e ano que vem pretendo cursar ${cursoFaculdade} na minha cidade natal.`);
+
+anoAtual= 2025;
+cidadeAtual = "Joiville";
+idade = 17;
+
